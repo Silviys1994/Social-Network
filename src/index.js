c6866b8eec408875e5598cdf06d8850c54871433
Silviys1994 <randex1994@gmail.com>
2023-03-29 18:27:03 +0300

    32. Уроки React JS - прокидываем callback через props/сделали колбек с новогопоста + пропсы с ним

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import state from "./redux/state.js";
+import { addPost } from "./redux/state.js";
 import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <React.StrictMode>
-      <App state={state} />
+      <App state={state} addPost={addPost} />
     </React.StrictMode>
   </HashRouter>
 );
