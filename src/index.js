e21aa0afddb143c6b18f81a112f8469f93da04a3
Silviys1994 <randex1994@gmail.com>
2023-04-10 15:19:20 +0300

    38. Уроки, Курс React JS - dispatch и action/ в стейте удадили адПост и апдейтНьюПост и заменили диспатчем

--- src/index.js
+++ src/index.js
@@ -12,11 +12,7 @@ const rerenderEntireTree = (state) => {
   root.render(
     <HashRouter>
       <React.StrictMode>
-        <App
-          state={state}
-          addPost={store.addPost.bind(store)}
-          updateNewPostText={store.updateNewPostText.bind(store)}
-        />
+        <App state={state} dispatch={store.dispatch.bind(store)} />
       </React.StrictMode>
     </HashRouter>
   );
--- src/redux/state.js
+++ src/redux/state.js
@@ -41,28 +41,32 @@ let store = {
       ],
     },
   },
+  _callSubscriber() {
+    console.log("state changed");
+  },
+
   getState() {
     return this._state;
   },
-  callSubscriber() {
-    console.log("state changed");
-  },
-  addPost() {
-    let newPost = {
-      id: 5,
-      message: this._state.profilePage.newPostText,
-      likesCount: 0,
-    };
-    this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = "";
-    this._callSubscriber(this._state);
-  },
-  updateNewPostText(newText) {
-    this._state.profilePage.newPostText = newText;
-    this._callSubscriber(this._state);
-  },
   subscribe(observer) {
     this._callSubscriber = observer;
+  },
+
+  dispatch(action) {
+    // {type: ADD-POST}
+    if (action.type === "ADD-POST") {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = "";
+      this._callSubscriber(this._state);
+    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
   },
 };
 // export const ;
